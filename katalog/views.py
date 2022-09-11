8cd7d913205a71e7f8e090e50a2b52c9f69e9b66
shafaleyda <shafaleyda@gmail.com>
2022-09-11 16:51:39 +0700

    First commit

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_catalog_item = CatalogItem.objects.all()
+    context = {
+        'list_catalog': data_catalog_item, 
+        'name': 'Shafa', 
+        'student_id': '2106634534'  
+        
+}
+    return render(request, 'katalog.html', context)
