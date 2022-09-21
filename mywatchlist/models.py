54f4b3d0248302233def5057673462caed55852b
shafaleyda <shafaleyda@gmail.com>
2022-09-22 00:17:55 +0700

    Adding CSS Styles

--- mywatchlist/models.py
+++ mywatchlist/models.py
@@ -3,18 +3,6 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class WatchlistItem(models.Model):
-    # WATCHED = 'Watched'
-    # NEVER_WATCHED = 'Have Not Watched'
-
-    # WATCHED_OR_NOT = [
-    #     (WATCHED, 'Watched'), 
-    #     (NEVER_WATCHED, 'Have Not Watched'), 
-    # ]
-    # watched = models.CharField(
-    #     max_length=20, 
-    #     choices=WATCHED_OR_NOT, 
-    #     default=NEVER_WATCHED,
-    # )
     watched = models.TextField()
     title = models.TextField()
     rating = models.FloatField()
--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,10 @@
+from multiprocessing.connection import Client
+from urllib import response
 from django.test import TestCase
 
+class UnitTestingMyWatchlist(TestCase):
+    def unit_testing_html(self):
+        response = Client.get('')
+        self.assertEqual(response.status_code,200)
+        
 # Create your tests here.
