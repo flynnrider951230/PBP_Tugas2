316ec0ef7b878c9ce09806047764c1ad38bf8772
shafaleyda <shafaleyda@gmail.com>
2022-09-22 00:45:00 +0700

    Adding unit test

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,10 +1,18 @@
 from multiprocessing.connection import Client
-from urllib import response
 from django.test import TestCase
+import requests
 
 class UnitTestingMyWatchlist(TestCase):
-    def unit_testing_html(self):
-        response = Client.get('')
+    def test_html(self):
+        response = requests.get('http://pbpshafa.herokuapp.com/mywatchlist/html/')
         self.assertEqual(response.status_code,200)
-        
+
+    def test_xml(self):
+        response = requests.get('http://pbpshafa.herokuapp.com/mywatchlist/xml/')
+        self.assertEqual(response.status_code,200)
+    
+    def test_json(self):
+        response = requests.get('http://pbpshafa.herokuapp.com/mywatchlist/json/')
+        self.assertEqual(response.status_code,200)
+
 # Create your tests here.
